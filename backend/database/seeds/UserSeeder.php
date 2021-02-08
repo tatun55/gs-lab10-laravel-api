b2b5f675aead041fce31fb1a0cce0d2eea69c3d0
tatun55 <tatun55@gmail.com>
2021-02-08 18:06:59 +0900

    jwt

---
+++ backend/database/seeds/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class)->create([
+            'name' => 'user',
+            'email' => 'user@dummy.com',
+        ]);
+    }
+}
